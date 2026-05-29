23c58beaa4f3628fbd026fc0929a8bac2fe1e99c
Vikas R <rvikas549@gmail.com>
2026-05-29 20:44:44 +0530

    Fixes

--- Server/config/firebase.js
+++ Server/config/firebase.js
@@ -1,9 +1,26 @@
 import admin from 'firebase-admin';
 
-import serviceAccount from '../serviceAccountKey.json' assert { type: 'json' };
+const serviceAccount = {
+  projectId:
+    process.env.FIREBASE_PROJECT_ID,
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+  clientEmail:
+    process.env.FIREBASE_CLIENT_EMAIL,
+
+  privateKey:
+    process.env.FIREBASE_PRIVATE_KEY?.replace(
+      /\\n/g,
+      '\n'
+    ),
+};
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential:
+      admin.credential.cert(
+        serviceAccount
+      ),
+  });
+}
 
 export default admin;
